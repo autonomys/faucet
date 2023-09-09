1830a85f2c1bbef392bd464fd051484942f85187
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-09-08 20:53:18 -0400

    fix saveAddress

--- smart_contract/scripts/utils/Faucet.ts
+++ smart_contract/scripts/utils/Faucet.ts
@@ -7,9 +7,22 @@ export const deploy = async (deployer: Signer, logAddress: boolean = true) => {
 
   await faucet.waitForDeployment()
   const faucetAddress = await faucet.getAddress()
+  const deploymentTransaction = faucet.deploymentTransaction()
+  if (deploymentTransaction != null) deploymentTransaction.wait(2)
 
   if (logAddress) {
-    await addressBook.saveContract('Faucet', faucetAddress, network.name, await deployer.getAddress())
+    addressBook.saveContract(
+      'Faucet',
+      faucetAddress,
+      network.name,
+      await deployer.getAddress(),
+      network.config.chainId,
+      deploymentTransaction?.blockHash != null ? deploymentTransaction?.blockHash : undefined,
+      deploymentTransaction?.blockNumber != null ? deploymentTransaction?.blockNumber : undefined,
+      'Faucet deployed by deploy script',
+      {},
+      true,
+    )
 
     console.log('Faucet deployed to:', faucetAddress)
   }
