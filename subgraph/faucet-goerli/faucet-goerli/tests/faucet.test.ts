67b960883328f45e137f03b446fcbe6e3ee9618f
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-11-25 18:23:19 -0500

    Initialize subgraph

---
+++ subgraph/faucet-goerli/faucet-goerli/tests/faucet.test.ts
@@ -0,0 +1,53 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { Deposit } from "../generated/schema"
+import { Deposit as DepositEvent } from "../generated/Faucet/Faucet"
+import { handleDeposit } from "../src/faucet"
+import { createDepositEvent } from "./faucet-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let from = Address.fromString("0x0000000000000000000000000000000000000001")
+    let amount = BigInt.fromI32(234)
+    let newDepositEvent = createDepositEvent(from, amount)
+    handleDeposit(newDepositEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("Deposit created and stored", () => {
+    assert.entityCount("Deposit", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "Deposit",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "from",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "Deposit",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "amount",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
