79b59dff5a6d3770945731e2be3e81b0ddd5a347
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-05-21 10:24:11 -0400

    add slack stats on request

--- web-app/src/pages/api/requestTokens.ts
+++ web-app/src/pages/api/requestTokens.ts
@@ -4,7 +4,7 @@ import { Client, FaunaHttpErrorResponseContent, query as faunaQuery } from 'faun
 import { NextApiRequest, NextApiResponse } from 'next'
 import { contracts } from '../../constants/contracts'
 import { networks } from '../../constants/networks'
-import { createStats, findStats, updateStats } from '../../utils'
+import { buildSlackStatsMessage, createStats, findStats, sendSlackMessage, updateStats } from '../../utils'
 
 type AccountType = 'github' | 'discord'
 
@@ -69,9 +69,27 @@ const incrementFaucetRequestsCount = async (address: string, accountType: Accoun
   const stats = await findStats(requestDate)
   console.log('stats', stats)
   if (!stats || stats === null || stats.length === 0) {
-    await createStats(address, accountType, requestDate)
+    const slackMessageId = await sendSlackMessage(
+      'Current week evm-faucet requests',
+      buildSlackStatsMessage('update', 1, 1)
+    )
+    await createStats(address, accountType, slackMessageId, requestDate)
   } else {
     const statsFound = stats[0].data
+    const isExistingAddresses = statsFound.addresses.find((a: string) => a === address)
+    await sendSlackMessage(
+      'Current week evm-faucet requests',
+      buildSlackStatsMessage(
+        'update',
+        statsFound.requests + 1,
+        isExistingAddresses ? statsFound.uniqueAddresses : statsFound.uniqueAddresses + 1,
+        {
+          ...statsFound.requestsByType,
+          [accountType]: statsFound.requestsByType[accountType] ? statsFound.requestsByType[accountType] + 1 : 1
+        }
+      ),
+      statsFound.slackMessageId
+    )
     await updateStats(stats[0].ref, accountType, statsFound, address)
   }
 }
