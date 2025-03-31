9f021f5792790fc3d917033f850526c6f879bceb
Naman Anand <iamnamananand996@gmail.com>
2025-04-01 01:21:32 +0530

    fix: 🐛 update description

--- web-app/components/Header.tsx
+++ web-app/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC = () => {
     <div className='mb-10 text-center'>
       <h2 className='text-4xl font-bold mb-4'>Faucet</h2>
       <p className='text-lg text-muted-foreground mb-2 dark:text-gray-400'>
-        Get free testnet tokens for building applications on the Autonomys Network.
+        Get testnet tokens for building applications on the Autonomys Network.
       </p>
       <Link href='https://docs.autonomys.network/mainnet/overview' target='_blank'>
         <span className='inline-flex items-center rounded-full border border-gray-300 dark:border-gray-700 px-3 py-1 text-sm font-medium cursor-pointer'>
