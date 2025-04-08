f9966a667695d5d02e9fe35bf7c971950035ebf6
Naman Anand <iamnamananand996@gmail.com>
2025-04-08 23:28:40 +0530

    feat: ✨ add persist to zustand store

--- web-app/store/useStore.ts
+++ web-app/store/useStore.ts
@@ -1,6 +1,7 @@
 // store/useStore.ts
 import { Network, networks } from '@autonomys/auto-utils'
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 export enum NetworkOptions {
   AUTO_EVM = 'auto-evm-taurus',
@@ -14,9 +15,17 @@ interface NetworkState {
   setNetworks: (networks: Network[]) => void
 }
 
-export const useNetworkStore = create<NetworkState>((set) => ({
-  network: NetworkOptions.AUTO_EVM,
-  setNetwork: (network: NetworkOptions) => set({ network }),
-  networks: networks,
-  setNetworks: (networks: Network[]) => set({ networks })
-}))
+export const useNetworkStore = create<NetworkState>()(
+  persist(
+    (set) => ({
+      network: NetworkOptions.AUTO_EVM,
+      setNetwork: (network: NetworkOptions) => set({ network }),
+      networks: networks,
+      setNetworks: (networks: Network[]) => set({ networks })
+    }),
+    {
+      name: 'network-storage',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
+)
