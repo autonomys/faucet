f009d94613cd468d72eacdfa7df6b68d5a846da4
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2025-10-08 13:50:17 -0400

    Update useStore and .env.sample

--- web-app/store/useStore.ts
+++ web-app/store/useStore.ts
@@ -4,7 +4,7 @@ import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
 export enum NetworkOptions {
-  AUTO_EVM = 'auto-evm-taurus'
+  AUTO_EVM = 'auto-evm-chronos'
 }
 
 interface NetworkState {
